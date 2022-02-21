e1244c519086a547a4a99cab39063c509414ee81
Keks <keks@htmlacademy.ru>
2022-02-21 19:18:03 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,17 @@
+function getSashaForkRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min < max && min >= 0) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } return 'Минимальное число в диапазоне не должно быть равно или больше максимального числа в этом диапазоне,а так же быть отрицательным.Пожалуйста,внесите коррективы.';
+}
+getSashaForkRandom(0, 100);
+/* https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
+
+
+function getMaxLengthStr (currentStr, maxLength) {
+  if (currentStr.length > maxLength) {
+    return false;
+  } return true;
+}
+getMaxLengthStr ('sasha', 20);
