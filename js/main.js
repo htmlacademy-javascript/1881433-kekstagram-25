cbc7d6b8d3511edf21cf28573b6d13f474c60639
alexander <kuyancev1985@mail.ru>
2022-03-20 20:13:05 +0300

    добавил import './form.js' в точку входа

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import './miniatures.js';
-//import {photos} from './util.js';
+/* eslint-disable no-unused-vars */
+import {photos} from './util.js';
+/* eslint-disable no-unused-vars */
+import './form.js';
 
 //console.log(photos);
