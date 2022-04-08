45daa102408f5d8384ba55af3d4aa205ac0482da
alexander <kuyancev1985@mail.ru>
2022-04-08 15:08:17 +0300

    добавил div и закомментировал оба ul чтобы не мешали

--- js/full-size-photo.js
+++ js/full-size-photo.js
@@ -1,34 +1,71 @@
-import {isEscapeKey, getRandomAvatarUrl, getRandomName, getRandomMessage, getRandomDescription} from './util.js';
+import {isEscapeKey, photos} from './util.js';
+
+//const commentsLoader = document.querySelector('.comments-loader');
+//const socialCommentCount = document.querySelector('.social__comment-count');
+const socialContainer = document.querySelector('.social__container');
+
+
+const addSocialContainer = () => {
+  const fullSocialCommentsFragment = document.createDocumentFragment();
+  for (let i = 0; i < photos.length; i++) {
+    const photo = photos[i];
+    const newUl = document.createElement('ul');
+    newUl.classList.add('social__comments');
+    for (let j = 0; j < photo.comments.length; j++) {
+      const comment = photo.comments[j];
+      const newLi = document.createElement('li');
+      newLi.classList.add('social__comment');
+      const newImg = document.createElement('img');
+      newImg.classList.add('social__picture');
+      const newP = document.createElement('p');
+      newP.classList.add('social__text');
+      newLi.appendChild(newImg);
+      newLi.appendChild(newP);
+      newUl.appendChild(newLi);
+      newImg.src = comment.avatar;
+      newImg.alt = comment.name;
+      newP.textContent = comment.message;
+      //newLi.classList.add('hidden');
+      fullSocialCommentsFragment.appendChild(newUl);
+    }
+  }
+  return socialContainer.appendChild(fullSocialCommentsFragment);
+};
 
 const bigPictureSection = document.querySelector('.big-picture');
 const body = document.querySelector('body');
 const miniPhotosList = document.querySelectorAll('.view');
 
-for (let i = 0; i < miniPhotosList.length; i++) {
-  const miniPhoto = miniPhotosList[i];
-  miniPhoto.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    const miniPictureSrc = miniPhoto.querySelector('.picture__img').src;
-    document.getElementById('full-photo').src = miniPictureSrc;
-    const miniatureLikes = miniPhoto.querySelector('.picture__likes').textContent;
-    document.querySelector('.likes-count').textContent = miniatureLikes;
-    const miniatureComments = miniPhoto.querySelector('.picture__comments').textContent;
-    document.querySelector('.comments-count').textContent = miniatureComments;
-    document.querySelector('.social__picture').src = getRandomAvatarUrl();
-    document.querySelector('.social__picture_2').src = getRandomAvatarUrl();
-    document.querySelector('.social__picture').alt = getRandomName();
-    document.querySelector('.social__picture_2').alt = getRandomName();
-    document.querySelector('.social__text').textContent = getRandomMessage();
-    document.querySelector('.social__text_2').textContent = getRandomMessage();
-    document.querySelector('.social__caption').textContent = getRandomDescription();
-    const socialCommentCount = document.querySelector('.social__comment-count');
-    const commentsLoader = document.querySelector('.comments-loader');
-    bigPictureSection.classList.remove('hidden');
-    body.classList.add('modal-open');
-    socialCommentCount.classList.add('hidden');
-    commentsLoader.classList.add('hidden');
+/*const findEachUlContent = () => {
+  const allNewUls = document.querySelectorAll('.social__comments');
+  const allNewLis = document.querySelectorAll('.social__comment');
+
+  allNewUls.forEach(element => {
+    console.log(element.children.length);
   });
-}
+  console.log(allNewUls.length, allNewLis.length);
+}*/
+
+const pressMiniPhoto = () => {
+  addSocialContainer();
+  for (let i = 0; i < miniPhotosList.length; i++) {
+    const miniPhoto = miniPhotosList[i];
+    miniPhoto.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      //findEachUlContent();
+      const miniPictureSrc = miniPhoto.querySelector('.picture__img').src;
+      document.getElementById('full-photo').src = miniPictureSrc;
+      const miniatureLikes = miniPhoto.querySelector('.picture__likes').textContent;
+      document.querySelector('.likes-count').textContent = miniatureLikes;
+      const miniatureComments = miniPhoto.querySelector('.picture__comments').textContent;
+      document.querySelector('.comments-count').textContent = miniatureComments;
+      bigPictureSection.classList.remove('hidden');
+      body.classList.add('modal-open');
+    });
+  }
+};
+pressMiniPhoto();
+
 
 document.addEventListener('keydown', (evt) => {
   if (isEscapeKey(evt)) {
--- js/main.js
+++
@@ -1,6 +0,0 @@
-import './miniatures.js';
-//import {photos} from './util.js';
-import './form.js';
-import './full-size-photo.js';
-
-//console.log(photos);
