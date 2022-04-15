973ae0968858cc184739ffeab2980cbdb58d3407
alexander <kuyancev1985@mail.ru>
2022-04-16 02:08:38 +0300

    убрал ненужное присваивание и всё ненужное

--- js/miniatures.js
+++ js/miniatures.js
@@ -7,7 +7,6 @@ const template = document.querySelector('#picture').content;
 const templateContent = template.querySelector('.picture');
 const fragment = document.createDocumentFragment();
 
-/* eslint-disable no-unused-vars */
 const addFeatures = () => {
   for (let i = 0; i < photos.length; i++) {
     const photo = photos[i];
@@ -18,13 +17,13 @@ const addFeatures = () => {
     liContainer.appendChild(clonedTemplateContent);
     clonedTemplateContent.querySelector('.picture__img').src = photo.url;
     clonedTemplateContent.querySelector('.picture__likes').innerHTML = photo.likes;
-    const countComments = clonedTemplateContent.querySelector('.picture__comments').innerHTML = photo.comments.length;
+    clonedTemplateContent.querySelector('.picture__comments').innerHTML = photo.comments.length;
   }
   fragment.appendChild(ulContainer);
   picturesContainer.appendChild(fragment);
   return picturesContainer;
 };
-/* eslint-disable no-unused-vars */
+addFeatures();
 
 //console.log(addFeatures(), photos);
 
